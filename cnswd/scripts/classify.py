dd5d5cc83a80f17dba342092ef70f93a0873a6f8
ldf <liudengfeng_sd@outlook.com>
2020-07-04 13:16:42 +0800

    + 命令行：分类树BOM

--- cnswd/scripts/classify.py
+++ cnswd/scripts/classify.py
@@ -6,16 +6,22 @@ from ..cninfo.classify_tree import ClassifyTree
 from ..mongodb import get_db
 
 db = get_db()
-collection_name = '股票分类'
-collection = db[collection_name]
 
 
 def refresh():
     # 覆盖式更新
-    collection.drop()
     s = time.time()
     with ClassifyTree() as api:
+        collection_name = '股票分类'
+        collection = db[collection_name]
+        collection.drop()
         for doc in api.yield_classify_tree():
             collection.insert_one(doc)
+        collection_name = '分类BOM'
+        collection = db[collection_name]
+        collection.drop()
+        docs = api.classify_tree_bom
+        collection.insert_many(docs)
+
     duration = time.time() - s
     api.logger.info(f"耗时{duration:.4f}秒")
