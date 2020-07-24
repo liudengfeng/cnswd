20ffaee5250db065836130242f2e6621142eefcf
LDF <liudengfeng_sd@outlook.com>
2020-07-24 19:22:59 +0800

    使用DB_HOST配置文件

--- cnswd/connect.py
+++ cnswd/connect.py
@@ -1,8 +1,9 @@
 from pymongo import MongoClient
+from .setting.constants import DB_HOST
 
 
 class Connect(object):
     @staticmethod
     def get_connection():
         # 使用默认值
-        return MongoClient("192.168.3.3", 27017)
+        return MongoClient(DB_HOST, 27017)
--- cnswd/utils/log_utils.py
+++ cnswd/utils/log_utils.py
@@ -1,13 +1,15 @@
 import logging
 from log4mongo.handlers import MongoHandler
+from ..setting.constants import DB_HOST
 
 
 def make_logger(name, collection='cnswd', level=logging.NOTSET):
-    formatter = logging.Formatter('%(asctime)s %(levelname)s %(name)s %(message)s')
+    formatter = logging.Formatter(
+        '%(asctime)s %(levelname)s %(name)s %(message)s')
     logger = logging.Logger(name)
     logger.addHandler(
         MongoHandler(level=level,
-                     host='localhost',
+                     host=DB_HOST,
                      database_name='eventlog',
                      collection=collection if collection else name,
                      fail_silently=False,
