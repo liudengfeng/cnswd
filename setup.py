2efcb8268958da4f742d7906ea2c871a61de277e
LDF <liudengfeng_sd@outlook.com>
2023-04-27 16:11:42 +0800

    +

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="cnswd",
-    version="5.0.0",
+    version="5.0.1",
     packages=find_packages(),
     long_description="""
     股票网络数据工具包
